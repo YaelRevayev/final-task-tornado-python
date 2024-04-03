4564e2e1275025350c7f5e79bf064e0911eb7829
YaelRevayev <gexploreapp@gmail.com>
2024-04-03 14:36:05 +0300

    changed the format of http req

--- send_files.py
+++ send_files.py
@@ -4,9 +4,10 @@ import requests
 import threading
 import redis
 from logger import logger,error_logger
+import constant
 
 
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_client = redis.StrictRedis(host=constant.REDIS_HOST_IP, port=constant.REDIS_HOST_PORT, db=0)
 
 def classifyFiles(filename):
     file_part = remove_extension(filename)[-1]
@@ -15,13 +16,17 @@ def classifyFiles(filename):
         threading.Thread(target=watch_for_second_part,args=(filename,)).start()
 
     elif file_part == 'b':
-        first_file = redis_client.get(filename[:-1] + 'ab')
-        files = [{"file": open(first_file, "rb")},{"file": open(filename, "rb")}]
-        #response = requests.post("http://localhost:8000/uploadfiles/", files=files)
-        #if response.status_code == 200:
-            #logger.info(f"File '{filename}' sent successfully.")
-        #else:
-            #error_logger.error(f"Error sending file '{filename}': {response.status_code} {response.reason}")
+        first_file_name = redis_client.get(filename[:-1] + 'ab')
+        files_to_send = []
+        files_to_send.append(("files", (first_file_name, read_file(first_file_name))))
+        files_to_send.append(("files", (filename, read_file(filename))))
+        response = requests.post("http://{ip}:{port}/merge_and_sign".format(ip = constant.HAPROXY_SERVER_IP,
+                                                                      port = constant.HAPROXY_SERVER_PORT),
+                                                                        files=files_to_send)
+        if response.status_code == 200:
+            logger.info(f"Files '{first_file_name}' , '{filename}' sent successfully.")
+        else:
+            error_logger.error(f"Error sending files '{first_file_name}' , '{filename}': {response.status_code} {response.reason}")
 
 def watch_for_second_part(file_a):
     time.sleep(60)
@@ -31,7 +36,13 @@ def watch_for_second_part(file_a):
             logger.info(f"File '{file_a}' deleted - couldn't find second part of the file.")
 
 def save_to_redis(filename):
-    redis_client.rpush(remove_extension(filename) + 'b', filename)
+    redis_client.set(remove_extension(filename) + 'b', filename)
+
+def read_file(filename):
+    with open(filename, "rb") as file:
+            file_data = file.read()
+
+    return file_data
 
 def remove_extension(filename):
     base_filename, _ = os.path.splitext(filename)
