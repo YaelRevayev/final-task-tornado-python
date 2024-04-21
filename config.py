e21c98a2290408212790fad093416fd4e6d05340
YaelRevayev <gexploreapp@gmail.com>
2024-04-21 13:00:23 +0300

    changed config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-HAPROXY_SERVER_IP = "10.12.0.9"
-HAPROXY_SERVER_PORT = 8000
+HAPROXY_SERVER_IP = "10.12.0.12"
+HAPROXY_SERVER_PORT = 80
 REDIS_HOST_IP = "localhost"
 REDIS_HOST_PORT = 6379
 DIRECTORY_TO_WATCH = "/home/pure-ftpd-server/final-task-tornado-python/files_output"
