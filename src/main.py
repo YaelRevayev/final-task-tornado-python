e0fbd6bc65077240671e52107cb52c629e28a6c3
YaelRevayev <gexploreapp@gmail.com>
2024-05-12 11:00:27 +0300

    check

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from files_listener import files_listener, listen_for_file_expiration
 import configs.config as config
+import multiprocessing
 
 
 def main():
-    listen_for_file_expiration()
+    multiprocessing.Process(
+        target=listen_for_file_expiration,
+        args=(),
+    ).start()
     print("meow")
     files_listener(config.DIRECTORY_TO_WATCH)
 
