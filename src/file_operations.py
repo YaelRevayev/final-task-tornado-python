267ae95eca461298fb5e4d234016b5df5630c682
YaelRevayev <gexploreapp@gmail.com>
2024-05-22 14:34:25 +0300

    tests run

--- src/file_operations.py
+++ src/file_operations.py
@@ -1,10 +1,12 @@
 import os
 from configs import config as config
+from logger import error_or_success_logger
 
 project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 
 def read_file(filename):
+    error_or_success_logger.debug("reading file...")
     with open("/{0}/{1}".format(project_dir, filename), "rb") as file:
         file_data = file.read()
     return file_data
@@ -27,4 +29,5 @@ def list_files(curr_file, first_file):
     files_to_send.append(
         ("files", (first_file, read_file(f"{config.FILES_FOLDER_NAME}/{first_file}")))
     )
+    error_or_success_logger.debug("listed files...")
     return files_to_send
