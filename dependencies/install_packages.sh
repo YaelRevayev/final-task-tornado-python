902ffa39b00c147b4c36db3279e66418f4db98ce
YaelRevayev <gexploreapp@gmail.com>
2024-05-27 15:04:01 +0300

    finished

--- dependencies/install_packages.sh
+++ dependencies/install_packages.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
-
 set -e
-
 # Update the package list
 sudo apt-get update
-
 # Install prerequisites for adding new repositories
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 sudo su
-# Download and install Filebeat
-sudo wget -P /home/ https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.16.3-amd64.deb
-
-cd /home/
-
-sudo dpkg -i filebeat-7.16.3-amd64.deb
-
 # Install packages listed in packages.txt
 sudo xargs -a packages.txt apt-get install -y
-
 sudo systemctl enable pure-ftpd
 sudo systemctl start pure-ftpd
-
 sudo systemctl enable redis-server
 sudo systemctl start redis-server
-
-
+# Download and install Filebeat
+sudo wget -P /home/ https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.16.3-amd64.deb
+cd /home/
+sudo dpkg -i filebeat-7.16.3-amd64.deb
 echo "All packages have been installed."
